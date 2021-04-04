c18bc30b5d74f910f0450795498cd60ab343f629
Rizwan Tejpar <natural-affinity@users.noreply.github.com>
2021-04-04 01:47:44 -0700

    event: 2020, day: 3, part: 1

---
+++ trajectory/trajectory.go
@@ -0,0 +1,56 @@
+package trajectory
+
+import (
+	"bufio"
+	"errors"
+	"os"
+
+	"github.com/natural-affinity/aoc/calendar"
+)
+
+var Problem = &calendar.Puzzle{Event: 2020, Desc: "Day 3: Toboggan Trajectory"}
+
+// Tree (# character)
+const Tree rune = 35
+
+type Trail struct {
+	Forest [][]rune
+}
+
+func (t *Trail) CountTrees() (int, error) {
+	x, y := 0, 0
+	h := len(t.Forest)
+	if h == 0 {
+		return 0, errors.New("empty map")
+	}
+
+	w := len(t.Forest[0])
+	count := 0
+	for y < h-1 {
+		x = (x + 3) % w
+		y += 1
+
+		if t.Forest[y][x] == Tree {
+			count += 1
+		}
+	}
+
+	return count, nil
+}
+
+func Scout(path string) (*Trail, error) {
+	fp, err := os.Open(path)
+	if err != nil {
+		return &Trail{}, errors.New("invalid forest map")
+	}
+
+	scanner := bufio.NewScanner(fp)
+	trail := &Trail{}
+	trail.Forest = [][]rune{}
+	for scanner.Scan() {
+		columns := []rune(scanner.Text())
+		trail.Forest = append(trail.Forest, columns)
+	}
+
+	return trail, nil
+}
---
+++ trajectory/trajectory_test.go
@@ -0,0 +1,64 @@
+package trajectory_test
+
+import (
+	"errors"
+	"path"
+	"reflect"
+	"testing"
+
+	"github.com/natural-affinity/aoc/trajectory"
+	"github.com/natural-affinity/gotanda"
+)
+
+func TestScout(t *testing.T) {
+	cases := []struct {
+		Name   string
+		Result *trajectory.Trail
+		Error  error
+	}{
+		{"invalid", &trajectory.Trail{}, errors.New("invalid forest map")},
+	}
+
+	for _, tc := range cases {
+		p := path.Join("testdata", tc.Name+".input")
+		result, err := trajectory.Scout(p)
+
+		r := !reflect.DeepEqual(result, tc.Result)
+		e := !gotanda.CompareError(err, tc.Error)
+
+		if e || r {
+			t.Errorf("\nTest: %s\nExpected:\n %v %s\nActual:\n %v %s",
+				tc.Name,
+				tc.Result, tc.Error,
+				result, err)
+		}
+	}
+}
+
+func TestCountTrees(t *testing.T) {
+	cases := []struct {
+		Name   string
+		Result int
+		Error  error
+	}{
+		{"map", 187, nil},
+		{"sample", 7, nil},
+		{"empty", 0, errors.New("empty map")},
+	}
+
+	for _, tc := range cases {
+		p := path.Join("testdata", tc.Name+".input")
+		trail, _ := trajectory.Scout(p)
+		result, err := trail.CountTrees()
+
+		r := !(result == tc.Result)
+		e := !gotanda.CompareError(err, tc.Error)
+
+		if e || r {
+			t.Errorf("\nTest: %s\nExpected:\n %d %s\nActual:\n %d %s",
+				tc.Name,
+				tc.Result, tc.Error,
+				result, err)
+		}
+	}
+}
