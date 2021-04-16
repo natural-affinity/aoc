c965d3fe5ba96766607878a607df3b0c322c5e42
Rizwan Tejpar <natural-affinity@users.noreply.github.com>
2021-04-16 02:09:34 -0700

    update names

--- haversack/haversack.go
+++ haversack/haversack.go
@@ -11,21 +11,21 @@ var TrimRegex = regexp.MustCompile(` bag(s)?\.?(\s|,\s)?(contain\s?)?(\d+)?`)
 var Colors = regexp.MustCompile(`(?P<color>[a-z]+\s[a-z]+){1}(?:\sbag)`)
 
 type Rules map[string]map[string]struct{}
-type Cache map[string]struct{}
+type Set map[string]struct{}
 
-func HasGold(color string, set Cache, sack Rules, found Cache) {
-	if _, ok := set["shiny gold"]; ok {
+func HasGold(color string, s Set, sack Rules, found Set) {
+	if _, ok := s["shiny gold"]; ok {
 		found[color] = struct{}{}
 		return
 	}
 
-	for c := range set {
+	for c := range s {
 		HasGold(color, sack[c], sack, found)
 	}
 }
 
 func Count(sack Rules, target string) int {
-	var found = make(Cache)
+	var found = make(Set)
 	for color, set := range sack {
 		HasGold(color, set, sack, found)
 	}
@@ -49,7 +49,7 @@ func Parse(path string) (Rules, error) {
 		colors := strings.Split(line, ",")
 		first := strings.TrimSpace(colors[0])
 
-		rules[first] = make(map[string]struct{})
+		rules[first] = make(Set)
 		for _, c := range colors[1:] {
 			color := strings.TrimSpace(c)
 			rules[first][color] = struct{}{}
